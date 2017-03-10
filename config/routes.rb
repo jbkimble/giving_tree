2d8b1f424bd706edc71ee9f13b48760434c80113
Jon Kimble <jonathanbkimble@gmail.com>
2017-03-10 10:12:15 -0700

    Add facility signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,26 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
 
-  get "/login", to: "sessions#new"
-  get "/signup", to: "user#c"
+  #
+  # get '/login', to: 'sessions#new'
+  # post '/login', to: 'sessions#create'
+  # delete '/logout', to: 'sessions#destroy'
+  #
+  # get '/dashboard', to: 'users#show'
+
+  namespace :facility do
+    get '/signup', to: 'users#new'
+    get '/signup', to: 'users#create'
+
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+
+    get '/dashboard', to: 'users#show'
+    get '/edit', to: 'users#edit'
+    post '/edit', to: 'users#update'
+
+    resources :dashboard, only: [:index]
+  end
+
 end
